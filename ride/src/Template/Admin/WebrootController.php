1d3cab5d529dafd158112121910fcdd249407362
karthik.github.io <karthiksenniappan76@gmail.com>
2018-04-04 09:08:27 +0530

    Remove from duplicated directory

--- ride/src/Template/Admin/WebrootController.php
+++
@@ -1,9 +0,0 @@
-<?php
-    namespace App\Controller;
-
-    use App\Controller\AppController;
-
-    class WebrootController extends AppController
-    {
-
-    }
--- ride/src/Template/Email/html/karthik.ctp
+++
@@ -1,1 +0,0 @@
-<h1 style="color:blue;" align=center>Ride one for smart ride</h1>
